725a630f3ecd8f4e4fc5a4accabf6f3e32d488e4
Mathias Monstrey <mathias@monstrey-technologies.be>
2020-10-04 16:33:25 +0200

    initial commit

---
+++ SpotServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bosdyn.Api;
+using Grpc.Core;
+
+namespace SpotServer
+{
+    class SpotAuthService : Bosdyn.Api.AuthService.AuthServiceBase
+    {
+        public override Task<GetAuthTokenResponse> GetAuthToken(GetAuthTokenRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetAuthTokenResponse {Header = new ResponseHeader {}, Status = GetAuthTokenResponse.Types.Status.Ok, Token = "test token"});
+            // return base.GetAuthToken(request, context);
+        }
+    }
+    
+    internal class Program
+    {
+        private const int Port = 443;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Bosdyn.Api.AuthService.BindService((new SpotAuthService())) },
+                Ports = { new ServerPort("localhost", Port, new SslServerCredentials(new List<KeyCertificatePair>())) }
+            };
+            server.Start();
+
+            Console.WriteLine("Spot mock-server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
